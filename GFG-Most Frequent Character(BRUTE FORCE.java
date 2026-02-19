e9d30011b0b729e10f4f3f170e352bc10ba97b26
Alok raj <156644544+raj45alok@users.noreply.github.com>
2026-02-19 15:45:59 +0530

    Update GFG-Most Frequent Character(BRUTE FORCE.java
    improved and added an approach

--- GFG-Most Frequent Character(BRUTE FORCE.java
+++ GFG-Most Frequent Character(BRUTE FORCE.java
@@ -26,3 +26,46 @@ class Solution {
         
     }
 }
+// Optimized code //
+/*package Strings;
+import java.util.Scanner;
+import java.util.Arrays;
+//Return the Character with maximum frequency in the string  by sliding window method
+public class MostFrequentCharacter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int len = str.length();
+        int freq=0;
+        char ans='*';
+        char []ch = new char[len];
+        ch=str.toCharArray();
+        Arrays.sort(ch);
+        int i=0,j=0,maxfreq=-1;
+        while(j<len)
+        {
+            if (ch[i]==ch[j]) j++;
+            else {
+                freq=j-i;
+                if(freq>maxfreq)
+                {
+                    maxfreq=freq;
+                   ans =ch[i];}
+                   i=j;
+                }
+            }
+
+        freq = len-i;
+        if(freq>maxfreq)
+        {
+            maxfreq=freq;
+            ans =ch[i];}
+
+        System.out.println(ans);
+
+
+
+    }
+
+}
+*/
